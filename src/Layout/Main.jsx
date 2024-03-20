f704095be069acaf572d05fccc4ec8d343a058a4
suvrodeb <suvrodev.cse.77@gmail.com>
2024-03-20 18:32:43 +0600

    Added back to Top

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,38 +1,36 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../Pages/CommonPage/Header/Header';
-import Footer from '../Pages/CommonPage/Footer/Footer';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-import ScrollToTop from 'react-scroll-to-top';
-import ScrollTop from './Refer/ScrollTop/ScrollTop';
-import { CustomChat, FacebookProvider } from 'react-facebook';
-import FacebookMsg from './Refer/FacebookMsg/FacebookMsg';
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Pages/CommonPage/Header/Header";
+import Footer from "../Pages/CommonPage/Footer/Footer";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+import ScrollToTop from "react-scroll-to-top";
+import ScrollTop from "./Refer/ScrollTop/ScrollTop";
+import { CustomChat, FacebookProvider } from "react-facebook";
+import FacebookMsg from "./Refer/FacebookMsg/FacebookMsg";
+import BackToTopButton from "./Refer/BackToTopButton/BackToTopButton";
 
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-            <div className='max-w-7xl mx-auto'>
-                <Outlet></Outlet>
-            </div>
-            <Footer></Footer>
-            <ScrollTop></ScrollTop>
-            <FacebookMsg></FacebookMsg>
+  return (
+    <div>
+      <Header></Header>
+      <div className="max-w-7xl mx-auto">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
 
+      <BackToTopButton></BackToTopButton>
+      <FacebookMsg></FacebookMsg>
 
-            {/* Messenger Start */}
-            {/* <MessengerCustomerChat
-                pageId="105831338849112"
-                appId="<APP_ID>"
-                htmlRef="<REF_STRING>"
-            /> */}
-            {/* Messenger End */}
-
-          
-            
+      {/* <div className="flex ">
+        <div className="bg-green-600">
+          <ScrollTop></ScrollTop>
         </div>
-    );
+        <div>
+          <FacebookMsg></FacebookMsg>
+        </div>
+      </div> */}
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Layout/Refer/BackToTopButton/BackToTopButton.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const BackToTopButton = () => {
+  const [backToTopButton, setBackToTopButton] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setBackToTopButton(true);
+      } else {
+        setBackToTopButton(false);
+      }
+    });
+  }, []);
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      {backToTopButton && (
+        <button
+          className="btn bg-[#0A7CFF] fixed bottom-[30px] left-[50px]"
+          onClick={scrollUp}
+        >
+          {" "}
+          <FaArrowUp className="text-[#FFFFFF]" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default BackToTopButton;
